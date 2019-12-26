637eaf6df0c2a2933c5e35bb20e5a095a80c5d6f
seunghwan.won <tmdghks24@naver.com>
2019-12-26 12:37:22 +0900

    ror test sample

---
+++ test/models/article_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class ArticleTest < ActiveSupport::TestCase
+  def test_the_truth
+    assert true
+  end
+  test "should not save article without title" do
+  article = Article.new
+  assert_not article.save, "Saved the article without a title"
+  end
+  test "should report error" do
+    # some_undefined_variable is not defined elsewhere in the testcase
+    assert_raises(NameError) do
+      some_undefined_variable
+    end
+  end
+  test "article_count" do
+    assert_equal  2, Article.count
+  end
+  test "find all by name" do
+    assert_equal 2, articles(:one, :two).length
+  end
+end
